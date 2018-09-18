61b9e0e5d76e1487d3dc372f57a08672782e0380
pierpaolopierpaoli <pierpaolopierpaoli@gmail.com>
2018-09-18 09:24:04 +0200

    studenti corsi

--- FinsaWeb/Controllers/HomeController.cs
+++ FinsaWeb/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CorsiOnline.Models;
-
+using CorsiOnline.Models.Database
 
 namespace CorsiOnline.Controllers
 {
