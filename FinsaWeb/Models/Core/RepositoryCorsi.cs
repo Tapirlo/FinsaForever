d43482011e1cfcf2df11326729c8a63de027cb8b
pierpaolopierpaoli <pierpaolopierpaoli@gmail.com>
2018-09-18 11:00:00 +0200

    nuove modifiche

---
+++ FinsaWeb/Models/Core/RepositoryCorsi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CorsiOnline.Models;
+using CorsiOnline.Models.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorsiOnline.Models.Core
+{
+    public class RepositoryCorsi:IRepositoryCorsi,IDisposable
+    {
+        public void Dispose()
+        {
+            contesto.Dispose();
+        }
+        private ContestoCorso contesto;
+
+        public RepositoryCorsi(ContestoCorso ctx)
+        {
+            contesto = ctx;
+        }
+
+        public IEnumerable<Corso> GetAllCorsi()
+        {
+            return contesto.Corsi.Include(x => x.StudentiCorsi).ThenInclude(x => x.StudenteNavigation).Include(x => x.DocentiCorsi).ThenInclude(x => x.DocenteNavigation).Include(x => x.MaterieCorsi).ToList();
+        }
+    }
+}
--- FinsaWeb/Models/Core/RepositoryCorsiTest.cs
+++ FinsaWeb/Models/Core/RepositoryCorsiTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using CorsiOnline.Models.Database;
-namespace CorsiOnline.Models.Database
+namespace CorsiOnline.Models.Core
 {
     public class RepositoryCorsiTest:IRepositoryCorsi
     {
@@ -13,10 +13,10 @@ namespace CorsiOnline.Models.Database
         private void CreaCorsiAdMinchiam()
         {
             corsi = new List<Corso>();
-            corsi.Add(new Corso { IDCorso = "1", Nome = "C# for dummies", DataInizio = new DateTime(2018, 6, 1), DataFine = new DateTime(2018, 9, 30)});
-            corsi.Add(new Corso { IDCorso = "2", Nome = "Java c++ per principianti", DataInizio = new DateTime(2017, 6, 1), DataFine = new DateTime(2017, 9, 30) });
-            corsi.Add(new Corso { IDCorso = "3", Nome = "Javascript for dummies", DataInizio = new DateTime(2016, 6, 1), DataFine = new DateTime(2016, 9, 30) });
-            corsi.Add(new Corso { IDCorso = "4", Nome = "Programmazione for dummies", DataInizio = new DateTime(2015, 6, 1), DataFine = new DateTime(2015, 9, 30) });
+            corsi.Add(new Corso { IDCorso = "1", Nome = "C# for dummies", DataInizio = new DateTime(2018, 6, 1), DataFine = new DateTime(2018, 9, 30), StudentiCorsi=new List<StudenteCorso>(), DocentiCorsi = new List<DocenteCorso>(), MaterieCorsi = new List<MateriaCorso>() });
+            corsi.Add(new Corso { IDCorso = "2", Nome = "Java c++ per principianti", DataInizio = new DateTime(2017, 6, 1), DataFine = new DateTime(2017, 9, 30), StudentiCorsi = new List<StudenteCorso>() , DocentiCorsi = new List<DocenteCorso>(), MaterieCorsi = new List<MateriaCorso>() });
+            corsi.Add(new Corso { IDCorso = "3", Nome = "Javascript for dummies", DataInizio = new DateTime(2016, 6, 1), DataFine = new DateTime(2016, 9, 30), StudentiCorsi = new List<StudenteCorso>() , DocentiCorsi = new List<DocenteCorso>(), MaterieCorsi = new List<MateriaCorso>() });
+            corsi.Add(new Corso { IDCorso = "4", Nome = "Programmazione for dummies", DataInizio = new DateTime(2015, 6, 1), DataFine = new DateTime(2015, 9, 30), StudentiCorsi = new List<StudenteCorso>(), DocentiCorsi = new List<DocenteCorso>(), MaterieCorsi = new List<MateriaCorso>() });
         }
         public IEnumerable<Corso> GetAllCorsi()
         {
