5fc5d7853c02c7b85c1c4a600fce01dc869cbda7
she91 <sheetal.massino@outlook.com>
2018-09-18 09:36:50 +0200

    nuova classe aula

---
+++ FinsaWeb/Models/Aula.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinsaWeb.Model
+{
+    public class Aula
+    {
+        [Key]
+        [Column("nomeaula")]
+        [StringLength(15)]
+        public string NomeAula { get; set; }
+        [Required]
+        [Column("numeroposti")]
+        public int NumeroPosti { get; set; }
+        [Required]
+        [Column("numerocomputer")]
+        public int NumeroComputer { get; set; }
+        [Required]
+        [Column("superficie")]
+        public int Superficie { get; set; }
+
+    }
+}
