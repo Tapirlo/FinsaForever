af2dee324ade0d59a60eb3e9692b5b97faaf57de
pierpaolopierpaoli <pierpaolopierpaoli@gmail.com>
2018-09-18 08:38:48 +0200

    namespace giusto

--- FinsaWeb/Models/Core/DocentiRepository.cs
+++ FinsaWeb/Models/Core/DocentiRepository.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using FinsaWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FinsaWeb.Models.Core
+namespace CorsiOnline.Models.Core
 {
     public interface DocentiRepository
     {
