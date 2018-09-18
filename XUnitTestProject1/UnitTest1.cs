0391176e6ba1a181d9364678d56628ec6569d29d
Luca Lanaro <lanaroc@libero.it>
2018-09-18 13:58:17 +0200

    Xunit inserisci corso

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,16 +1,49 @@
-using System;
 using Xunit;
+using System.Linq;
+using System;
+using CorsiOnline.Models.Database;
+using CorsiOnline.Models;
+using Xunit.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace XUnitTestProject1
 {
-    public class UnitTest1
+    public class UnitTest1 : IDisposable
     {
-        [Fact]
-        public void Test1()
+        ContestoCorso _context;
+        IRepositoryCorsi repoCorsi;
+        ITestOutputHelper output;
+
+        public UnitTest1(ContestoCorso corso)
         {
-            //ciao mondo
-            int x = 0;
-            x = 2;
+            _context = corso;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        [Fact]
+        public void InserisciCorso()
+        {
+            using (IDbContextTransaction transaction = _context.Database.BeginTransaction())
+            {
+                int CountCorsiIniziale = _context.Corsi.Count();
+                Corso corsi = new Corso()
+                {
+                    Nome = "Test_Ciccio",                    
+                    IDCorso = "10",                    
+                    DataInizio = new DateTime(2018, 09, 01),
+                    DataFine = new DateTime(2018, 12, 01)
+
+                };
+                repoCorsi.AggiungiCorso(corsi);
+                Assert.Equal(CountCorsiIniziale + 1, _context.Corsi.Count());
+                _context.Remove(corsi);
+                _context.SaveChanges();
+            }
         }
     }
 }
