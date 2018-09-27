592b0e59a78cebcaef915a366d1bb46360e4ad1b
Amerigo <amefanclub@hotmail.it>
2018-09-27 09:52:34 +0200

    Create Api per docenti

--- FinsaWeb/ViewModels/DocenteModel.cs
+++ FinsaWeb/ViewModels/DocenteModel.cs
@@ -9,13 +9,25 @@ namespace CorsiOnline.ViewModels
 {
     public class DocenteModel
     {
-        public Docente UnDocente { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Email { get; set; }
+        public string CodiceFiscale { get; set; }
+        public string Telefono { get; set; }
+        public DateTime DataNascita { get; set; }
 
-        public DocenteModel()
+        public DocenteModel(Docente d)
         {
-            UnDocente = new Docente();
+            //UnDocente = new Docente();
+            Nome = d.Nome;
+            Cognome = d.Cognome;
+            Email = d.Email;
+            CodiceFiscale = d.CodiceFiscale;
+            Telefono = d.Telefono;
+            DataNascita = d.DataNascita;
         }
 
+        public Docente UnDocente { get; set; }
         public String Materie { get; set; }
 
         public IEnumerable<Insegnamento> RegistraMaterie()
