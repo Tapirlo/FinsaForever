561de338cb728bbff2531d8ca5babd3fd91df149
Luca Lanaro <lanaroc@libero.it>
2018-10-02 11:08:48 +0200

    Api Studenti

---
+++ FinsaWeb/Controllers/StudenteApiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CorsiOnline.Models;
+using CorsiOnline.ViewModels;
+using CorsiOnline.Models.Database;
+
+namespace CorsiOnline.Controllers
+{
+    [Route("api/studenti")]
+    public class StudenteApiController : Controller
+    {
+        private IStudenteRepository repository;
+
+        public StudenteApiController(IStudenteRepository repo)
+        {
+            repository = repo;
+        }
+        [HttpGet("AllStudenti")]
+        public IActionResult AllStudenti()
+        {
+
+
+            return Ok(repository.GetAllStudenti().Select(x => new StudenteModels(x)));
+
+        }
+
+        
+    }
+}
---
+++ FinsaWeb/ViewModels/StudenteModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CorsiOnline.Models.Database;
+
+namespace CorsiOnline.ViewModels
+{
+    public class StudenteModels
+    {
+        public string CodiceFiscale { get; set; }
+        public String Nome { get; set; }
+        public String Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+        public String Sesso { get; set; }
+
+        public override string ToString()
+        {
+            return CodiceFiscale + " " + Nome;
+        }
+        public StudenteModels()
+        {
+
+        }
+
+        public StudenteModels(Studente s)
+        {
+            CodiceFiscale = s.CodiceFiscale;
+            Nome = s.Nome;
+            Cognome = s.Cognome;
+            DataNascita = s.DataNascita;
+            Sesso = s.Sesso;         
+
+            
+
+        }
+    }
+}
